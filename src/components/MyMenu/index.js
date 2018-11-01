0147e06f846849056c86ee36fe437da4c7884698
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2018-11-01 21:45:36 +0700

    menu v1

--- src/components/MyMenu/index.js
+++ src/components/MyMenu/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'antd';
+// import { Menu, Icon } from 'antd';
+
+import './menu.css';
 
 class MyMenu extends Component {
   render() {
     return (
-      <div>
-        <a>Home</a>
-        <a>CV</a>
+      <div className="menu">
+        <a href="/">Home</a>
+        <a href="/">CV</a>
       </div>
     );
   }
