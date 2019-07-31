c65533b0291906f2cfdd2b00b0e6c58ab5cb6997
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2019-07-31 22:06:51 +0700

    saved

--- src/App.js
+++ src/App.js
@@ -6,23 +6,15 @@ import MyHeader from './components/MyHeader';
 import MyContent from './components/MyContent';
 
 const {
-  Footer, Content,
+  Footer,
 } = Layout;
 
 class App extends Component {
   render() {
     return (
-      // <Layout>
-      //   <MyHearder/>
-      //   <MyContent/>
-      //   <Footer>Footer</Footer>
-      // </Layout>
       <Layout>
         <MyHeader />
-        <Content>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-          <MyContent/>
-        </Content>
+        <MyContent/>
         <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
--- src/components/MyMenu/index.js
+++ src/components/MyMenu/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Menu } from 'antd';
 
+import './menu.css';
+
 class MyMenu extends Component {
   render() {
     return (
@@ -10,8 +12,8 @@ class MyMenu extends Component {
         defaultSelectedKeys={['1']}
         style={{ lineHeight: '64px' }}
       >
-        <Menu.Item key="1" style={{ width: 100 }}>Home</Menu.Item>
-        <Menu.Item key="2" style={{ width: 100 }}>CV</Menu.Item>
+        <Menu.Item key="1" className="menu-item">Home</Menu.Item>
+        <Menu.Item key="2" className="menu-item">CV</Menu.Item>
       </Menu>
     );
   }
