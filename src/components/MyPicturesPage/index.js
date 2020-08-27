85893f24ee5d776fece3316844f0e00e407efcac
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-08-27 22:50:09 +0700

    saved button background on pictures page

--- src/components/MyPicturesPage/index.js
+++ src/components/MyPicturesPage/index.js
@@ -1,5 +1,26 @@
 import React from 'react'
 
-const MyPicturesPage = () => <div>This is my pictures page</div>
+import './my-picture-page.scss'
+
+const MyPicturesPage = () => (
+  <div className="wrapper">
+    <div className="inner">
+      <div className="content">
+        <h1>Radius</h1>
+        <h2>
+          A fully responsive masonry-style
+          <br />
+          portfolio template.
+        </h2>
+        <a href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+          <span>Lets Go</span>
+        </a>
+      </div>
+      {/* <a href="#" class="button hidden">
+        <span>Let's Go</span>
+      </a> */}
+    </div>
+  </div>
+)
 
 export default MyPicturesPage
