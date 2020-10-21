78bd66029ba6aacff314a3eb999aae2dbc2af378
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-10-21 22:06:35 +0700

    revert font

--- src/components/MyPicturesPage/index.js
+++ src/components/MyPicturesPage/index.js
@@ -2,23 +2,29 @@ import React from 'react'
 
 import './my-picture-page.scss'
 
-const MyPicturesPage = () => (
-  <div className="wrapper">
-    <div className="inner">
-      <div className="content">
-        <h1>Pictures</h1>
-        <h2>
-          Son pictures gallery
-          <br />
-        </h2>
-        <div>
-          <span>Lets Go</span>
-        </div>
+const BigButton = () => (
+  <div className="inner">
+    <div className="content">
+      <h1>Pictures</h1>
+      <h2>
+        Son pictures gallery
+        <br />
+      </h2>
+      <div>
+        <span>Lets Go</span>
       </div>
-      {/* <a href="#" class="button hidden">
+    </div>
+    {/* <a href="#" class="button hidden">
         <span>Let's Go</span>
       </a> */}
-    </div>
+  </div>
+)
+
+// const SmallButton = () => <div />
+
+const MyPicturesPage = () => (
+  <div className="wrapper">
+    <BigButton />
   </div>
 )
 
